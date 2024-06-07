711c9be4812e9a72467bfa55e005f443aa29147e
satriaqd12 <satriamufindra@gmail.com>
2024-06-07 20:18:30 +0700

    Add files via upload

---
+++ Soal 1.py
@@ -0,0 +1,19 @@
+print("Robot siap beroperasi")
+
+# Program ini akan menampilkan teks Robot Bergerak Maju    
+print("Robot Bergerak Maju")
+# Setelah kode di jalankan akan ada tulisan/teks di terminal
+
+Kecepatan = 10
+print(Kecepatan)
+
+Arah = "kiri"
+kecepatan =5
+Durasi =10 
+print("Robot Bergerak Ke",Arah)
+print("Kecepatan:",kecepatan)
+print("Durasi:",Durasi,"detik")
+
+status1 = "Robot Aktif"
+Status2 = "Menunggu Perintah"
+print(status1,"dan",Status2)
