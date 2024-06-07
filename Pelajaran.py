fe4a0974d149b0a500c4f0fa2586464f7de7ea0a
satriaqd12 <satriamufindra@gmail.com>
2024-06-07 20:41:06 +0700

    Add files via upload

---
+++ Pelajaran.py
@@ -0,0 +1,23 @@
+# list with int as element's data type
+list_1 = [2, 4, 8, 16]
+
+# list with str as elemnt's data type
+list_2 = ["grayson", "jason", "Tim", "Damian"]
+
+# list with various data type in the element
+list_3 = [24, False, "Hello Python"]
+
+list_4 = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"]
+
+Nama_depan = "Satria"
+Nama_Belakang = "Mufindra"
+Agama = "Islam"
+Menikah = "False"
+
+Profil = [Nama_depan, Nama_Belakang, Agama, Menikah]
+
+print(Profil)
+print(list_1[2])
+print(list_2[3])
+print(list_3[0])
+print(list_4[2])
